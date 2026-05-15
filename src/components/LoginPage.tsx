d4429a9548a2ac8dfcb63f64a453d2e01a6e8ad4
sarangan16 <mailsarangan@gmail.com>
2026-05-15 10:25:33 +0200

    Loginpage useStates added, handlelogin working

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -3,6 +3,25 @@ import {supabase} from "../lib/supabase"
 
 
 export default function LoginPage(){
+
+
+   const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  async function handleLogin() {
+    setLoading(true);
+    setError("");
+
+    const { error } = await supabase.auth.signInWithPassword({ email, password });
+
+    if (error) {
+      setError(error.message);
+    }
+
+    setLoading(false);
+  }
   return(
     <div className="min-h-screen bg-slate-900 flex items-center justify-center p-4">
       <div className="bg-slate-800 rounded-2xl p-8 w-full max-w-md">
@@ -11,23 +30,41 @@ export default function LoginPage(){
           login to board
 
         </p>
+         {error && (
+          <p className="text-red-400 text-sm mb-4">{error}</p>
+        )}
+
 
         <div className="space-y-4">
         <div>
           <label className="text-slate-400 text-xs block mb-1">Email</label>
-          <input type="email" placeholder="Your Email" className="w-full bg-slate-700 text-white rounded-lg px-3 py-2" />
+          <input type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)} 
+            placeholder="Your Email" 
+            className="w-full bg-slate-700 text-white rounded-lg px-3 py-2" />
         </div>
 
         <div>
           <label className="text-slate-400 text-xs block mb-1">
             Password
           </label>
-          <input type="password" placeholder="****" className="w-full bg-slate-700 text-white round-lg px-3 py-2 text-sm" />
+          <input type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="****" className="w-full bg-slate-700 text-white round-lg px-3 py-2 text-sm" />
         </div>
         </div>
-        <button className="w-full mt-6 py-2.5 bg-yellow-400 text-black font-semibold rounded-lg text-sm hover:bg-yellow-400 transition">
 
+         <button
+          onClick={handleLogin}
+          disabled={loading}
+          className="w-full mt-6 py-2.5 bg-yellow-500 text-black font-semibold rounded-lg text-sm hover:bg-yellow-400 transition"
+        >
+          {loading ? "Logging in..." : "Log In"}
         </button>
+
+
         <p className="text-slate-400 text-xs text-center mt-4">
           No account? {" "}
           <a href="" className="text-yellow-400 hover:underline">
